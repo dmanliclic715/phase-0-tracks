e1585b93b629b8b836d041aa2c73ae5df0380dfc
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-08 20:58:59 -0500

    Add release 3 multiple employees requirements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,15 @@
-# Employee Intake Form
+#LOOP
+valid_employee_num = 0
+puts "How Many Employees will be filling out this form?"
+employee_num = gets.chomp.to_i
+until valid_employee_num == employee_num
+#LOOP
 puts "What is your name?"
 employee_name = gets.chomp
 puts "How old are you?"
 employee_age = gets.chomp.to_i
 puts "What year were you born?"
-employee_yob = 2016-gets.chomp.to_i
+employee_yob = (2016 - gets.chomp.to_i)
 valid_garlic_pref = false
 until valid_garlic_pref
   puts "Our company cafeteria serves garlic bread. Should we order some for you?(y or n)"
@@ -31,15 +36,19 @@ until valid_insurance_pref
 end
 
 #Conditionals
-if (employee_age == employee_yob) && garlic_pref=="y" || insurance_pref=="y"
-puts "Probably not a vampire"
-#I'm having a lot of trouble distinguishing between lines 37-39. I feel like I'm missing something or I didn't express my variables accurately. Since the two conditionals are similar they return only the "Probably a vampire" string no matter what I do.
-elsif (employee_age != employee_yob) &&garlic_pref=="n" or insurance_pref=="n"
-puts "Probably a vampire"
-elsif (employee_age != employee_yob) &&garlic_pref == "n" && insurance_pref == "n"
-  puts "Almost certainly a vampire"
-  # I also can't get this piece of code to work either. I'll input these names but it'll still result with "Probably not a vampire" with the conditionals.
-elsif employee_name = "Drake Cula" || "Tu Fang"
+if (employee_name =="Drake Cula" || employee_name =="Tu Fang") && (employee_age ==employee_yob) && garlic_pref == "y" && insurance_pref == "y"
   puts "Definitely a Vampire"
+elsif (employee_age ==employee_yob) && (garlic_pref =="y" && insurance_pref =="n") || (garlic_pref =="n" && insurance_pref =="y")
+  puts "Probably not a vampire"
+elsif (employee_yob !=employee_age) && (garlic_pref =="y" && insurance_pref=="n") || (garlic_pref =="n" && insurance_pref =="y")
+  # for the "(garlic_pref =="n" && insurance_pref =="y")" conditional above I keep getting the result as "probably not a vampire" even though the employee_age and the employee_yob don't match. How is this possible?
+  puts "Probably a vampire"
+elsif (employee_age !=employee_yob) && garlic_pref =="n" && insurance_pref =="n"
+  puts "Almost certainly a vampire"
 else puts "Results Inconclusive"
 end
+#LOOP END#
+valid_employee_num += 1
+end
+#LOOP END#
+
