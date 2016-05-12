e6072261e0c2ab85eec28df3aeea0e32a7d21b3d
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-11 21:41:27 -0500

    Complete release 0 and release 1

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,22 @@
+chicago = []
+p chicago
+chicago = ["Bean", "deepdish pizza", "The L", "Sears Tower", "Wrigleyville"]
+p chicago
+chicago.delete("The L")
+p chicago
+chicago.insert(2, "Portillo's")
+p chicago
+chicago.delete("Bean")
+p chicago
+
+include = chicago.include?("The L")
+ if include == true
+  p "Chicago includes The L"
+ else
+  p "Chicago does not include The L"
+ end
+
+paris = ["Eiffel Tower", "The Louvre", "Mona Lisa"]
+p paris
+chicaris = chicago + paris
+p chicaris
