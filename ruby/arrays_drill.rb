9d211a7339c902bfa4b649181414937644104704
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-11 22:06:06 -0500

    Complete Release 2 requirements

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,23 +1,39 @@
 # Paired with Andrew Jung
-chicago = []
-p chicago
-chicago = ["Bean", "deepdish pizza", "The L", "Sears Tower", "Wrigleyville"]
-p chicago
-chicago.delete("The L")
-p chicago
-chicago.insert(2, "Portillo's")
-p chicago
-chicago.delete("Bean")
-p chicago
+def array_method(x, y, z)
+  p [x, y, z]
+end
 
-include = chicago.include?("The L")
- if include == true
-  p "Chicago includes The L"
- else
-  p "Chicago does not include The L"
- end
+def add_to_array(arr, value)
+  p arr.push(value)
+end
 
-paris = ["Eiffel Tower", "The Louvre", "Mona Lisa"]
-p paris
-chicaris = chicago + paris
-p chicaris
+
+
+
+# chicago = []
+# p chicago
+# chicago = ["Bean", "deepdish pizza", "The L", "Sears Tower", "Wrigleyville"]
+# p chicago
+# chicago.delete("The L")
+# p chicago
+# chicago.insert(2, "Portillo's")
+# p chicago
+# chicago.delete("Bean")
+# p chicago
+
+# include = chicago.include?("The L")
+#  if include == true
+#   p "Chicago includes The L"
+#  else
+#   p "Chicago does not include The L"
+#  end
+
+# paris = ["Eiffel Tower", "The Louvre", "Mona Lisa"]
+# p paris
+# chicaris = chicago + paris
+# p chicaris
+
+
+array_method(1, "two", nil)
+add_to_array([], "a")
+add_to_array(["a", "b", "c", 1, 2], 3)
