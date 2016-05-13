aa041777ed604d82eb2267d7adf0c56bcd2e3a5d
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-12 22:55:12 -0500

    Add pseudocode of release 3

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+# 1. Ask the client for their name
+#   1.1 Allow client to input their name as a string
+# 2. Ask the client for their age
+#   2.1 Allow client to input their age
+#   2.2 Convert client's input into an integer
+# 3. Ask the client how many children they have
+#   3.1 Allow client to input how many children they have
+#   3.2 Convert the client's input into an integer
+#   3.4 If the client does not have children, allow the client to proceed without input. Or have their input of 0 equivalent to nil.
+# 4. Ask the client what decor theme they prefer
+#   4.1 Allow client to input data and have it become a string
+# 5. Ask client if they are allergic to any colors
+#   5.1 Allow client to input their answer
+#   5.1 If it they say yes have the data inputed as a true boolean
+#   5.2 If they say no have the data inputed as a false boolean
