11e7cf81f78e7c6915a90f4db8f60627208dfba8
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-14 13:09:05 -0500

    Add and complete step 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,17 +1,44 @@
 #Paired with Mark Kairuz
 # Write your own method that takes a block
-def message
- puts "This is an example of running a block through a method"
- yield("pizza", "hamburger", "KFC")
- puts "The above is the result"
+# def message
+#  puts "This is an example of running a block through a method"
+#  yield("pizza", "hamburger", "KFC")
+#  puts "The above is the result"
+# end
+# message { |food1, food2, food3| puts "My favorite foods are #{food1}, #{food3} and #{food2}" }
+
+
+# Step 1
+make_of_car = ["Ford", "Toyota", "Honda"]
+make_model_of_car = {:Ford => "taurus", :Toyota => "corolla", :Honda => "civic"}
+
+# Step 2
+# Using .each on array
+puts "Original data:"
+p make_of_car
+puts "Below is using the .each block:"
+make_of_car.each do |item|
+  p item + " is the best car company"
 end
-message { |food1, food2, food3| puts "My favorite foods are #{food1}, #{food3} and #{food2}" }
+puts "After .each call:"
+p  make_of_car
 
+# Using .map! on array
+puts "Original Data:"
+p make_of_car
+make_of_car.map! do |item|
+  p item + " is the best car company"
+end
+puts "After .map! call:"
+p make_of_car
 
-# #Step 1
-# make_of_car = ["Ford", "Toyota", "Honda"]
-# make_model_of_car = {:Ford => "taurus", :Toyota => "corolla", :Honda => "civic"}
+#Using .each on the hash
+puts "Original hash:"
+p make_model_of_car
 
-# #Step 2
-# puts "Original data:"
-# p make_of_car
+make_model_of_car.each do |make, model|
+  puts "#{make} makes the #{model}"
+end
+
+puts "After .each on hash:"
+p make_model_of_car
