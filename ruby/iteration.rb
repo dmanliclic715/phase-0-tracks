c0d5f3efebf4180448a363b4104d0851ab491fd0
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-14 12:38:31 -0500

    Add Write your own method that takes a block

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,17 @@
 #Paired with Mark Kairuz
-make_of_car = ["Ford", "Toyota", "Honda"]
-make_model_of_car = {:Ford => "taurus", :Toyota => "corolla", :Honda => "civic"}
+# Write your own method that takes a block
+def message
+ puts "This is an example of running a block through a method"
+ yield("pizza", "hamburger", "KFC")
+ puts "The above is the result"
+end
+message { |food1, food2, food3| puts "My favorite foods are #{food1}, #{food3} and #{food2}" }
 
+
+# #Step 1
+# make_of_car = ["Ford", "Toyota", "Honda"]
+# make_model_of_car = {:Ford => "taurus", :Toyota => "corolla", :Honda => "civic"}
+
+# #Step 2
+# puts "Original data:"
+# p make_of_car
