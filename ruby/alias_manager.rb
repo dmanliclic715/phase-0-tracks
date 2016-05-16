f5a1c0b85c7841bd0bb8b1000386def5c8e6db41
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-16 18:03:42 -0500

    Attempt to complete solo chalange

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,23 +6,33 @@
 # 5. Iterate through the array selecting all of the vowels and change them into the next vowels
 # 6. Iterate through the new array and change all the consonants to the next consonant.
 # 7. Make the array of characters into a new string with two words
-
-def x(name)
-  name_arr = name.split(' ')
-  name_arr[0], name_arr[1] = name_arr[1], name_arr[0]
-  name_swap = name_arr.join(' ')
-  name_split = name_swap.split('')
-  name_split.map! {|item| item.next}
-  final_name = name_split.join('')
-  final_name.gsub('!', ' ')
+valid_input = false
+until valid_input
+  puts "*** Welcome to the Alias Assigner ***"
+  puts "Input Name:"
+    def alias_machine(name)
+    name_arr = name.split(' ')
+    name_arr[0], name_arr[1] = name_arr[1], name_arr[0]
+    name_swap = name_arr.join(' ')
+    name_split = name_swap.split('')
+    name_split.map! {|item| item.next}
+    final_name = name_split.join('')
+    final_name.gsub('!', ' ')
+      if name == "quit"
+        exit
+      end
+    end
+  assigned_alias = alias_machine(gets.chomp)
+  puts "Your New Medieval Nordic alias is : #{  assigned_alias}!"
+  alias_storage = []
+  alias_storage << assigned_alias
+  p alias_storage
 end
-puts "Felicia Torres"
-p x("Felicia Torres")
 
-def next_vowel(ary)
-  ary.map! { |item| item.next}
-  p ary
-end
+#Could not implement Release 2 Requirements
+
+
+
 
 
 
