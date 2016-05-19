697836c9d2b3801aa4497fdc9828af2c7118430d
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-19 14:17:54 -0500

    Add Release 0 requriements

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# Species --------------------
+# Pup-a-saurus
+
+# Characteristics ----------------------
+# Stance: Walks on hind legs
+# Approachabilty: Very approachable
+# Bark: Sounds like tiny dinosaur rawrs
+# Fur color: Greenish brown
+# Forearms: Small forearms like that of a t-rex
+
+# Behavior -----------------------------
+# Hyperactive
