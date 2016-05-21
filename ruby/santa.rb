5c4c7c22d448d46c6f93708d80406b5716936405
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-20 20:27:46 -0500

    Add do the thing to release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,6 +3,8 @@ class Santa
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -10,8 +12,31 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!"
   end
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  age = 0
+
+  def celebrate_birthday
+     puts "I am now #{@age + 1} years old"
+  end
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking << @reindeer_ranking.delete(reindeer_name)
+  end
+
+  # Getter Methods
+  def age
+    @age
+  end
+  def ethnicity
+    @ethnicity
+  end
+  def reindeer_ranking
+    @reindeer_ranking
+  end
+  def gender
+    @gender
+  end
+  # Setter Methods
+  def gender=(new_gender)
+    @gender = new_gender
+  end
 end
 
 # Chris_Kringle = Santa.new
@@ -27,10 +52,18 @@ end
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
-santas = []
-genders = ["male", "female", "type of gender", "female", "female", "male", "N/a", "prefer not to say"]
-ethnicity = ["Nigerian", "Chinese", "Finnish", "Brazilian", "Cuban", "Laotian","Hmong", "N/a", "prefer not to say"]
-ethnicity.length.times do |input|
-  santas << Santa.new(genders[input], ethnicity[input])
-end
-p santas
+# santas = []
+# genders = ["male", "female", "type of gender", "female", "female", "male", "N/a", "prefer not to say"]
+# ethnicity = ["Nigerian", "Chinese", "Finnish", "Brazilian", "Cuban", "Laotian","Hmong", "N/a", "prefer not to say"]
+# ethnicity.length.times do |input|
+#   santas << Santa.new(genders[input], ethnicity[input])
+# end
+# p santas
+
+bob = Santa.new("m","american")
+p bob
+p bob.celebrate_birthday
+bob.get_mad_at("Donner")
+bob.gender = "f"
+
+
