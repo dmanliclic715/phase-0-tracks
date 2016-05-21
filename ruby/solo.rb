0d20edc91cadb53aa1a9fdaee5fe4eadd80d3df5
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-21 12:46:29 -0500

    Add three methods to the medical worker class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,10 @@
 # Design Class: Medical Staff
 # 3 Attributes
-  # occupation
+  # occupation, department, age, and whether person has a stehoscope witht he default being "true"
+# 3 Methods
+  # A life saving method in which the healthcare workers shocks the patient and saves a life
+  # A resuscitate method in which an integer is inputted and that is how many times the  healtcare worker will pump during cpr
+  # A method in which the healthcare worker gives the patients meds
 class Medical_Staff
   def initialize(occupation, department, age)
     puts "Initializing Medical Personnel..."
@@ -9,15 +13,22 @@ class Medical_Staff
     @age = age
     @stethoscope = true
   end
-
-
-
-
-
+  def save_life
+    puts "CLEAAR! **saves life**"
+  end
+  def cpr(number_of_times)
+    number_of_times.times do
+    puts "**pump...chest**"
+    end
+    puts "I feel a pulse!"
+  end
+  def administer_meds
+    puts "**Gives patient medication**"
+  end
 
 
 
 end
 
 bob = Medical_Staff.new("doctor", "ER", 40)
-p bob
+bob.administer_meds
