d95f529761837e2c01724d6d36ffbbe51aefc5a2
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-21 19:04:51 -0500

    Complete and finish release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,34 @@
 # Release 1:Write a Simple Module
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
+# p Shout.yelling_happily("HOW ARE YOU")
+# p Shout.yell_angrily("What did you do?")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    puts words + "!!!" + " :("
   end
-  def self.yelling_happily(words)
-    words + "!!!" + " :D"
+  def yelling_happily(words)
+    puts words + "!!!" + " :B"
   end
 end
-p Shout.yelling_happily("HOW ARE YOU")
-p Shout.yell_angrily("What did you do?")
+
+class Girlfriend
+  include Shout
+end
+class Bestfriend
+  include Shout
+end
+
+ bae = Girlfriend.new
+ bae.yell_angrily("You did what?")
+
+ bff = Bestfriend.new
+ bff.yelling_happily("It's so good to see you")
+
