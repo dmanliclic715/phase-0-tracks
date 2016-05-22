7482f141a92fd5fa1a52dd3d0f23ce11073b0014
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-21 21:30:05 -0500

    Complete Release 3 and the requirements

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,2 +1,18 @@
-class Todolist
+class TodoList
+  def initialize(x)
+    @x = x
+  end
+  def get_items
+    @x
+  end
+  def add_item(y)
+    @x << y
+  end
+  def delete_item(z)
+    @x.delete(z)
+  end
+  def get_item(index)
+  @x[index]
+  end
+
 end
