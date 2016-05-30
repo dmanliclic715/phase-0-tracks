19416cb763507599bd2f97e6702362ba82f4e5d4
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-29 22:55:26 -0500

    Add release 1 requirements

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,9 @@ names.push("joe");
 
 console.log(colors);
 console.log(names);
+
+var horses = {};
+for (var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+console.log(horses);
