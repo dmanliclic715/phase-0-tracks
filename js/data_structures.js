f5a18d9e23b788f08bceaa1cd92eb941c5bc2141
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-29 23:15:53 -0500

    Add Release 2 requirements

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,17 @@ for (var i = 0; i < names.length; i++) {
   horses[names[i]] = colors[i];
 }
 console.log(horses);
+
+
+function Car(year, make, goodCondition) {
+this.year = year;
+this.make = make;
+this.goodCondition = goodCondition;
+this.honk = function() { console.log("HONK! HONK!"); };
+console.log("CAR INITIALIZATION COMPLETE")
+}
+
+var carOne = new Car(2001, 'Mitsubishi', false);
+var carTwo = new Car(1969, 'Chevrolet', true);
+carOne.honk();
+carTwo.honk();
