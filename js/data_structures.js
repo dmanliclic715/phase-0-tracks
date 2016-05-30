a9415131925dc95f430eee6520a420ec59c05a2c
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-29 22:16:48 -0500

    Add release 0 requirements

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "yellow", "red", "purple"];
+var names = ["bob", "earl", "jim", "mark"];
+
+colors.push("green");
+names.push("joe");
+
+console.log(colors);
+console.log(names);
