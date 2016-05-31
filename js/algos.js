2ebbdbdc0b058c42029d8d87dcc2ad220f7cfe9a
dmanliclic715 <dmanliclic715@gmail.com>
2016-05-30 22:16:00 -0500

    Attempt Release 0 requirements

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+ // write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+ //  Release 0: Find the Longest Phrase Pseudocode
+ //  1. Accept the array as an argument
+ //  2. Iterate through the array
+//    2.1 The iteration will count the number of characters in each item of the array.
+//    2.2 Compare the numbers of each item
+//    2.3 The number that has the highest number of characters will be printed
+testArray = ["long phrase","longest phrase","longer phrase"]
+function longPhrase(array) {
+    for (i = 0; i < array.length; i++) {
+      if (array[i].length > array[i].length) {
+        console.log(array[i]);
+      }
+    };
+}
+longPhrase(testArray);
+
+// I'm not too sure where to proceed from here
+
+
